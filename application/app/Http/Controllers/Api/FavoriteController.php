40f468fbb9160e85a3307b6a4c47bd5fd098b8b1
eduardoagas <eagasilva12@gmail.com>
2024-11-30 18:14:34 -0300

    all working test ok

--- application/app/Http/Controllers/Api/FavoriteController.php
+++ application/app/Http/Controllers/Api/FavoriteController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Services\FavoriteService;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FavoritesRequest;
 
